54237ee4df6675c1b89314540c496c3dbdf683d0
dfreeman3102 <davidfreeman3102@gmail.com>
2024-02-24 14:51:51 -0500

    allows start button to set questions and anwers on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,48 @@
+// makes changes to the question and answers
+var body = document.body;
+
+var questionEl = document.getElementById("question");
+var a1E1 = document.createElement("button");
+var a1E2 = document.createElement("button");
+var a1E3 = document.createElement("button");
+var a1E4 = document.createElement("button");
+
+
+function setQuestion() {
+    questionEl.textContent = "Question 1"
+    a1E1.textContent = "Answer 1"
+    a1E2.textContent = "Answer 2"
+    a1E3.textContent = "Answer 3"
+    a1E4.textContent = "Answer 4"
+
+    body.appendChild(a1E1);
+    body.appendChild(a1E2);
+    body.appendChild(a1E3);
+    body.appendChild(a1E4);
+}
+
 //Starts the questions and timer
 var startEl = document.getElementById("start-btn");
 
-function startQuiz(){
+function startQuiz() {
     startEl.setAttribute("style", "display:none");
     setTime();
 }
 
-startEl.addEventListener("click",startQuiz);
+startEl.addEventListener("click", startQuiz);
+startEl.addEventListener("click", setQuestion);
 
 //function to make timer countdown
 var timerEl = document.getElementById("timer");
 
 var timeLeft = 180;
 
-function setTime(){
+function setTime() {
     timerEl.setAttribute("style", "display: content");
 
-    var timerInterval = setInterval(function(){
+    var timerInterval = setInterval(function () {
         timeLeft--;
         timerEl.textContent = "Time Remaining: " + timeLeft;
-    },1000);
+    }, 1000);
 
-    if(answer === false){
-        timeLeft - 15;
-    }
-
-}
-
-
-// makes changes to the question and answers
-var questionEl = document.getElementById("question");
-
-function setQuestion(){
-    var question
 }
