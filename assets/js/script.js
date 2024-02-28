33703eb036db8c712ef6937b6775368c5bf2f21c
dfreeman3102 <davidfreeman3102@gmail.com>
2024-02-27 22:42:51 -0500

    added actual questions and answers, removed placeholder q & a's

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,57 +2,129 @@
 // This has all the questions with their respective answers, including true/false values
 var questions = [
     {
-        question: "Question one example?",
+        question: "What is the window object?",
         answers: [
-            { text: "Blan 1", correct: false },
-            { text: "ble 2", correct: true },
-            { text: "ble 3", correct: false },
-            { text: "ble 4", correct: false },
+            { text: "The lowest item in DOM hierarchy", correct: false },
+            { text: "The DOM", correct: false },
+            { text: "The screen size", correct: false },
+            { text: "The browser's window", correct: true },
         ]
     },
     {
-        question: "Question two example?",
+        question: "How do you traverse to the body in the DOM?",
         answers: [
-            { text: "Answer 1", correct: true },
-            { text: "Answer 2", correct: false },
-            { text: "Answer 3", correct: false },
-            { text: "Answer 4", correct: false },
+            { text: "document.body", correct: true },
+            { text: "body.document", correct: false },
+            { text: "document.window.body", correct: false },
+            { text: "window.body.document", correct: false },
         ]
     },
     {
-        question: "Question three example?",
+        question: "How do you change attributes in JS?",
         answers: [
-            { text: "Answer 1", correct: false },
-            { text: "Answer 2", correct: false },
-            { text: "Answer 3", correct: true },
-            { text: "Answer 4", correct: false },
+            { text: "setAttribute.example(`x`,`y`", correct: false },
+            { text: "example.appendAttribute()", correct: false },
+            { text: "example.setAttribute(`x`,`y`)", correct: true },
+            { text: "appendAttribute.example()", correct: false },
         ]
     },
     {
-        question: "Question four example?",
+        question: "How do you add something to the page in JS?",
         answers: [
-            { text: "Answer 1", correct: false },
-            { text: "Answer 2", correct: false },
-            { text: "Answer 3", correct: false },
-            { text: "Answer 4", correct: true },
+            { text: "body.document.addChild(example)", correct: false },
+            { text: "document.body.addChild(example)", correct: false },
+            { text: "body.document.appendChild(example)", correct: false },
+            { text: "document.body.appendChild(example)", correct: true },
         ]
     },
     {
-        question: "Question five example?",
+        question: "What is the purpose of setInterval and clearInterval for a timer?",
         answers: [
-            { text: "Answer 1", correct: false },
-            { text: "Answer 2", correct: true },
-            { text: "Answer 3", correct: false },
-            { text: "Answer 4", correct: false },
+            { text: "setInterval=stop, clearInterval=start", correct: false },
+            { text: "setInterval=start, clearInterval=stop", correct: true },
+            { text: "setInterval=display, clearInterval=hidden", correct: false },
+            { text: "setInterval=hidden, clearInterval=display", correct: false },
         ]
     },
     {
-        question: "Question six example?",
+        question: "What is the purpose of `example.addEventListener(`click`, example)`?",
         answers: [
-            { text: "Answer 1", correct: true },
-            { text: "Answer 2", correct: false },
-            { text: "Answer 3", correct: false },
-            { text: "Answer 4", correct: false },
+            { text: "When you click that item, a function happens", correct: true },
+            { text: "When you click that item, nothing happens", correct: false },
+            { text: "When you click any item, a function happens", correct: false },
+            { text: "When you click any item, nothing happens", correct: false },
+        ]
+    },
+    {
+        question: "What is the purpose of preventDefault() on a submit button?",
+        answers: [
+            { text: "It has no purpose", correct: false },
+            { text: "It stops it from clearing items", correct: false },
+            { text: "It stops all code from executing", correct: false },
+            { text: "Stops page from refreshing", correct: true },
+        ]
+    },
+    {
+        question: "Why is `keydown` important",
+        answers: [
+            { text: "So you can tell what button is being pressed", correct: true },
+            { text: "So you can see what button is not being pressed", correct: false },
+            { text: "So you can tell what time is left", correct: false },
+            { text: "It is not important", correct: false },
+        ]
+    },
+    {
+        question: "What does stopPropagation do?",
+        answers: [
+            { text: "It does not exist", correct: false },
+            { text: "It stops event bubbling to child event handlers", correct: false },
+            { text: "It stops event bubbling to parent event handlers", correct: true },
+            { text: "It changes attributes of the child element", correct: false },
+        ]
+    },
+    {
+        question: "What makes data attributes important?",
+        answers: [
+            { text: "Data attributes allow you to change something once", correct: false },
+            { text: "Data attributes allow you to contain constantly changing info", correct: true },
+            { text: "Nothing is important about it", correct: false },
+            { text: "It changes nothing about the element", correct: false },
+        ]
+    },
+    {
+        question: "What is the purpose of setItem and getItem",
+        answers: [
+            { text: "These are used in storing and accessing localStorage", correct: true },
+            { text: "They allow you to access globalStorage", correct: false },
+            { text: "They access eventListeners for you", correct: false },
+            { text: "They do nothing", correct: false },
+        ]
+    },
+    {
+        question: "What is JSON.stringify()?",
+        answers: [
+            { text: "It has no purpose", correct: false },
+            { text: "It converts JS strings to objects", correct: false },
+            { text: "It makes strings appear on the page", correct: false },
+            { text: "It converts JS objects to strings", correct: true },
+        ]
+    },
+    {
+        question: "What does JSON.parse() do?",
+        answers: [
+            { text: "It converts an h1 to an h2", correct: false },
+            { text: "It converts JS objects to strings", correct: false },
+            { text: "It stops all code from executing", correct: false },
+            { text: "It converts JS strings to objects", correct: true },
+        ]
+    },
+    {
+        question: "What is the purpose of setItem and getItem",
+        answers: [
+            { text: "These are used in storing and accessing localStorage", correct: true },
+            { text: "They allow you to access globalStorage", correct: false },
+            { text: "They access eventListeners for you", correct: false },
+            { text: "They do nothing", correct: false },
         ]
     },
 ]
